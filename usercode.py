9baa9f51b56d7089915d8a48937e5d4f335923a3
Artem-Buriak <95652125+Artem-Buriak@users.noreply.github.com>
2023-09-03 20:33:35 +0300

    Create usercode.py

---
+++ usercode.py
@@ -0,0 +1,21 @@
+import jwt
+from cryptography.hazmat.primitives import serialization
+
+payload_data = {
+    "iat": 1696083454,
+    "exp": 1696083454,
+    "jti": "d793ccbb",
+    "application_id": "ba6a8954-46aa-4f7e-beda-7c77ed718285"
+}
+
+key = open('private.key').read()
+
+token = jwt.encode(
+    payload=payload_data,
+    key=key,
+    algorithm='RS256'
+)
+
+
+def handle():
+    return token
